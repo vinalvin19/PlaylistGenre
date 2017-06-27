f76b41c3fe4aac3fb2fdf4a46e8b770d8d78b66d
Alvin Mustafa <alvinmustafa94@gmail.com>
2017-06-27 15:10:50 +0700

    Added Index ActionResult

---
+++ PlaylistGenreMvc/PlaylistGenreMvc/Controllers/GenreController.cs
@@ -0,0 +1,34 @@
+﻿using PlaylistGenreMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlaylistGenreMvc.Controllers
+{
+    public class GenreController : Controller
+    {
+        private OperationDataContext context;
+        public GenreController()
+        {
+            context = new OperationDataContext();
+        }
+
+        public ActionResult Index()
+        {
+            List<GenreModel> genreList = new List<GenreModel>();
+            var query = from genre in context.Genres select genre;
+            var genres = query.ToList();
+
+            foreach (var genreItem in genres)
+            {
+                genreList.Add(new GenreModel()
+                {
+                    Id = genreItem.Id,
+                    Name = genreItem.Name
+                });
+            }
+            return View(genreList);
+        }
+}
