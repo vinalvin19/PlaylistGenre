9d58b8037d801e445f351e80f5a54b5e0b96051f
Alvin Mustafa <alvinmustafa94@gmail.com>
2017-06-29 15:02:52 +0700

    Added Song Controller, Create and Index ActionResults

---
+++ PlaylistGenreMvc/PlaylistGenreMvc/Controllers/SongController.cs
@@ -0,0 +1,79 @@
+﻿using PlaylistGenreMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlaylistGenreMvc.Controllers
+{
+    public class SongController : Controller
+    {
+        private OperationDataContext context;
+
+        public SongController()
+        {
+            context = new OperationDataContext();
+        }
+
+        public ActionResult Index()
+        {
+            IList<SongModel> SongList = new List<SongModel>();
+            var query = from song in context.Songs
+                        join genre in context.Genres
+                        on song.GenreId equals genre.Id
+                        select new SongModel
+                        {
+                            Id = song.Id,
+                            Title = song.Title,
+                            Singer = song.Singer,
+                            Year = song.Year,
+                            Writer = song.Writer,
+                            GenreName = genre.Name
+                        };
+            SongList = query.ToList();
+
+            return View(SongList);
+        }
+
+        public ActionResult Create()
+        {
+            SongModel model = new SongModel();
+            PreparePublisher(model);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(SongModel model)
+        {
+            try
+            {
+                Song song = new Song()
+                {
+                    Title = model.Title,
+                    Singer = model.Singer,
+                    Writer = model.Writer,
+                    Year = model.Year,
+                    GenreId = model.GenreId
+                };
+                context.Songs.InsertOnSubmit(song);
+                context.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        private void PreparePublisher(SongModel model)
+        {
+            model.Genres = context.Genres.AsQueryable<Genre>().Select(x =>
+                    new SelectListItem()
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    });
+        }
+    }
+}
---
+++ PlaylistGenreMvc/PlaylistGenreMvc/Models/SongModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlaylistGenreMvc.Models
+{
+    public class SongModel
+    {
+        public SongModel()
+        {
+            Genres = new List<SelectListItem>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Singer { get; set; }
+        public string Year { get; set; }
+        public string Writer { get; set; }
+        [DisplayName("Genre")]
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+        public IEnumerable<SelectListItem> Genres { get; set; }
+    }
+}
